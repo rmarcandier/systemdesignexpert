eeeec000a1e539fd357440582f660576303950d9
rengonca <rengonca@cisco.com>
2020-10-31 00:38:30 +0000

    add leader_election

--- replication_and_sharding/aedb_proxy.js
+++ replication_and_sharding/aedb_proxy.js
@@ -1,31 +1,31 @@
 const axios = require('axios');
 const express = require('express');
 
-const SHARD_ADDRESS = ['http://localhost:3000', 'http://localhost:3001'];
-const SHARD_COUNT = SHARD_ADDRESSES.lenght;
+const SHARD_ADDRESSES = ['http://localhost:3000', 'http://localhost:3001'];
+const SHARD_COUNT = SHARD_ADDRESSES.length;
 
 const app = express();
 app.use(express.json());
 
 function getShardEndpoint(key) {
-    const shardNumber = key.charCodAt(0) % SHARD_COUNT;
+    const shardNumber = key.charCodeAt(0) % SHARD_COUNT;
     const ShardAddress = SHARD_ADDRESSES[shardNumber];
     return `${ShardAddress}/${key}`;
 }
 
 app.post('/:key', (req, res) => {
-    const getShardEndpoint = getShardEndpoint(req.params.key);
-    console.log(`Forwarding to: ${getShardEndpoint}`);
+    const shardEndpoint = getShardEndpoint(req.params.key);
+    console.log(`Forwarding to: ${shardEndpoint}`);
     axios
-        .post(getShardEndpoint, req.body)
+        .post(shardEndpoint, req.body)
         .then(innerRes => {
             res.send();
         });
 });
 
 app.get('/:key', (req, res) => {
-    const getShardEndpoint = getShardEndpoint(req.params.key);
-    console.log(`Forwarding to: ${getShardEndpoint}`);
+    const shardEndpoint = getShardEndpoint(req.params.key);
+    console.log(`Forwarding to: ${shardEndpoint}`);
     axios
         .get(shardEndpoint)
         .then(innerRes => {
@@ -38,5 +38,5 @@ app.get('/:key', (req, res) => {
 });
 
 app.listen(8000, () => {
-    console.log('Listening on port 80000!');
+    console.log('Listening on port 8000!');
 })
